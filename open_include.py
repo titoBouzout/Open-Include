476b7632d8e7e7db5051a9ebd39547cbaf7b359f
tito <tito.bouzout@gmail.com>
2011-10-24 20:54:08 -0300

    Improvements: check paths between quotes, selected text or current line. Resolve relative to view, project folders or absolute path.

--- open_include.py
+++ open_include.py
@@ -3,31 +3,62 @@ import os.path
 import re
 
 class OpenInclude(sublime_plugin.TextCommand):
-	def run(self, view):
+	def run(self, edit):
 		for region in self.view.sel():
+			opened = False
+
+			# between quotes
 			syntax = self.view.syntax_name(region.begin())
-			if re.match(".*string.quoted.double", syntax): self.doOpen(self.view, region, '"') # Match & Select Doubles
-			if re.match(".*string.quoted.single", syntax): self.doOpen(self.view, region, "'") # Match & Select Singles
+			if re.match(".*string.quoted.double", syntax) or re.match(".*string.quoted.single", syntax):
+				opened = self.do_open(self.view.substr(self.view.extract_scope(region.begin())))
 
-	def doOpen(self, view, region, char):
-		begin = region.begin() - 1
-		end = region.begin()
-		while view.substr(begin) != char or view.substr(begin - 1) == '\\': begin -= 1
-		while view.substr(end) != char or view.substr(end - 1) == '\\': end += 1
-		view.sel().subtract(region)
-		view.sel().add(sublime.Region(begin + 1, end))
+			# selected text
+			if not opened:
+				opened = self.do_open(self.view.substr(sublime.Region(region.begin(), region.end())))
 
-		for region in self.view.sel():
-			if region.empty():
-				line = self.view.line(region)
-				filepath = self.view.substr(line).strip()
-			else:
-				filepath = self.view.substr(region).strip()
+			# current line
+			if not opened:
+				opened = self.do_open(self.view.substr(sublime.Region(self.view.line(region.begin()).begin(), self.view.line(region.begin()).end())))
 
-			filepath = os.path.dirname(self.view.file_name().strip()) + '\\' + filepath.strip()
-			
-			if os.path.exists(filepath):
-				self.view.window().open_file(filepath)
-				sublime.status_message("Opening file " + filepath)
+	def do_open(self, path):
+		# remove quotes
+		path = re.sub('^"|\'', '', re.sub('^"|\'', '', path.strip()))
+		path = re.sub('"|\'$', '', re.sub('^"|\'$', '', path.strip()))
 
-		view.sel().clear()
+		# remove :row:col
+		path = re.sub('(\:[0-9]*)+$', '', path.strip()).strip()
+		print path
+
+		if path == '':
+			return False
+
+		# relative to view
+		if self.view.file_name() != None and self.view.file_name() != '':
+			maybe_path = self.resolve_relative(os.path.dirname(self.view.file_name()), path)
+			if os.path.isfile(maybe_path):
+				self.view.window().open_file(maybe_path)
+				sublime.status_message("Opening file " + maybe_path)
+				return True
+
+		# relative to project folders
+		for maybe_path in sublime.active_window().folders():
+			maybe_path = self.resolve_relative(maybe_path, path)
+			if os.path.isfile(maybe_path):
+				self.view.window().open_file(maybe_path)
+				sublime.status_message("Opening file " + maybe_path)
+				return True
+
+		#absolute
+		if os.path.isfile(path):
+			self.view.window().open_file(path)
+			sublime.status_message("Opening file " + path)
+			return True
+
+		return False
+
+	def resolve_relative(self, absolute, path):
+		subs = path.replace('\\', '/').split('/')
+		for sub in subs:
+			if sub != '':
+				absolute = os.path.join(absolute, sub)
+		return absolute
