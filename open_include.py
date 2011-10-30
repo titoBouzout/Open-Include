03764b7c58044c7f6144f46e2a13cd41d6780286
tito <tito.bouzout@gmail.com>
2011-10-30 09:14:11 -0300

    Will look into parent of view dirname. some clean-ups for clarity

--- open_include.py
+++ open_include.py
@@ -3,6 +3,8 @@ import os.path
 import re
 
 class OpenInclude(sublime_plugin.TextCommand):
+
+	# run and look for different sources of paths
 	def run(self, edit):
 		window = sublime.active_window()
 		view = self.view
@@ -12,17 +14,18 @@ class OpenInclude(sublime_plugin.TextCommand):
 			# between quotes
 			syntax = self.view.syntax_name(region.begin())
 			if re.match(".*string.quoted.double", syntax) or re.match(".*string.quoted.single", syntax):
-				opened = self.do_open(window, view, view.substr(view.extract_scope(region.begin())))
+				opened = self.resolve_path(window, view, view.substr(view.extract_scope(region.begin())))
 
 			# selected text
 			if not opened:
-				opened = self.do_open(window, view, view.substr(sublime.Region(region.begin(), region.end())))
+				opened = self.resolve_path(window, view, view.substr(sublime.Region(region.begin(), region.end())))
 
 			# current lines
 			if not opened:
-				opened = self.do_open(window, view, view.substr(sublime.Region(view.line(region.begin()).begin(), view.line(region.end()).end())))
+				opened = self.resolve_path(window, view, view.substr(sublime.Region(view.line(region.begin()).begin(), view.line(region.end()).end())))
 
-	def do_open(self, window, view, paths):
+	# resolve the path of these sources and send to try_open
+	def resolve_path(self, window, view, paths):
 
 		paths = paths.split('\n')
 		opened = False
@@ -39,30 +42,43 @@ class OpenInclude(sublime_plugin.TextCommand):
 			# relative to view
 			if view.file_name() != None and view.file_name() != '':
 				maybe_path = self.resolve_relative(os.path.dirname(view.file_name()), path)
-				if os.path.isfile(maybe_path):
-					window.open_file(maybe_path)
-					sublime.status_message("Opening file " + maybe_path)
-					opened = True
+				opened = self.try_open(window, maybe_path)
+				if opened:
+					continue
+
+			# relative to view dirname
+			if view.file_name() != None and view.file_name() != '':
+				maybe_path = self.resolve_relative(os.path.dirname(os.path.dirname(view.file_name())), path)
+				opened = self.try_open(window, maybe_path)
+				if opened:
 					continue
 
 			# relative to project folders
 			for maybe_path in sublime.active_window().folders():
 				maybe_path = self.resolve_relative(maybe_path, path)
-				if os.path.isfile(maybe_path):
-					window.open_file(maybe_path)
-					sublime.status_message("Opening file " + maybe_path)
-					opened = True
+				opened = self.try_open(window, maybe_path)
+				if opened:
 					break
-
-			#absolute
-			if os.path.isfile(path):
-				window.open_file(path)
-				sublime.status_message("Opening file " + path)
-				opened = True
+			if opened:
 				continue
+
+			# absolute
+			opened = self.try_open(window, path)
+			if opened:
+					continue
 
 		return opened
 
+	# try opening the resouce
+	def try_open(self, window, maybe_path):
+		if os.path.isfile(maybe_path):
+			window.open_file(maybe_path)
+			sublime.status_message("Opening file " + maybe_path)
+			return True
+		else:
+			return False
+
+	# util
 	def resolve_relative(self, absolute, path):
 		subs = path.replace('\\', '/').split('/')
 		for sub in subs:
